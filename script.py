7bf6a27ce7eb71399a5db26674d5cdadde81c4b4
Sbas0611 <dev.arboleda@gmail.com>
2023-09-27 16:24:14 -0500

    optimizated script

--- script.py
+++ script.py
@@ -1,18 +1,42 @@
 import json
 import pandas as pd
+import glob
+import concurrent.futures
 
-# Lee el archivo Excel en un DataFrame de pandas
-# Reemplaza 'archivo.xlsx' con el nombre de tu archivo Excel
-excel_file = 'data/colores.xlsx'
-df = pd.read_excel(excel_file)
+# Define el directorio donde se encuentran tus archivos .xlsx
+# Reemplaza con la ubicación de tus archivos .xlsx
+directorio = 'data'
 
-# Convierte el DataFrame a una lista de diccionarios
-json_data = df.to_dict(orient='records')
+# Utiliza glob para buscar todos los archivos .xlsx en el directorio
+archivos_xlsx = glob.glob(f'{directorio}/*.xlsx')
 
-# Guarda la lista de diccionarios en un archivo JSON
-# Reemplaza 'archivo.json' con el nombre que desees para el archivo JSON
-json_file = 'archivo.json'
-with open(json_file, 'w', encoding='utf-8') as f:
-    json.dump(json_data, f, ensure_ascii=False, indent=4)
+# Función para procesar un archivo .xlsx y convertirlo a JSON
 
-print(f'Se ha creado el archivo JSON: {json_file}')
+
+def procesar_xlsx(archivo_xlsx):
+    try:
+        # Lee el archivo .xlsx en un DataFrame de pandas
+        df = pd.read_excel(archivo_xlsx)
+
+        # Convierte el DataFrame a una lista de diccionarios
+        json_data = df.to_dict(orient='records')
+
+        # Define el nombre del archivo JSON de salida
+        nombre_json = archivo_xlsx.replace('.xlsx', '.json')
+
+        # Guarda la lista de diccionarios en un archivo JSON
+        with open(nombre_json, 'w', encoding='utf-8') as f:
+            json.dump(json_data, f, ensure_ascii=False, indent=4)
+
+        return f'Se ha creado el archivo JSON: {nombre_json}'
+    except Exception as e:
+        return f'Error al procesar {archivo_xlsx}: {str(e)}'
+
+
+# Procesa los archivos en paralelo para mayor eficiencia
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    resultados = list(executor.map(procesar_xlsx, archivos_xlsx))
+
+# Imprime los resultados
+for resultado in resultados:
+    print(resultado)
